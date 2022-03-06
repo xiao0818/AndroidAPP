02a5a7e03861b17ad6e91a78452ad83154073c59
Eddie <kta23379745@gmail.com>
2022-03-06 20:45:11 +0800

    finish Hello Constraint for HW2

--- HW2/app/src/main/java/com/example/android/MainActivity.java
+++ HW2/app/src/main/java/com/example/android/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,12 +15,16 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
     private TextView mShowCount;
+    private Button zeroButton;
+    private Button countButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        zeroButton = (Button) findViewById(R.id.button_zero);
+        countButton = (Button) findViewById(R.id.button_count);
     }
 
     public void showToast(View view) {
@@ -24,9 +32,23 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
+    public void clearToZero(View view) {
+        mCount = 0;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+        view.setBackgroundColor(getResources().getColor(R.color.gray));
+        countButton.setBackgroundColor(getResources().getColor(R.color.purple_500));
+    }
+
     public void countUp(View view) {
         mCount++;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+        if (mCount % 2 == 0){
+            view.setBackgroundColor(getResources().getColor(R.color.orange));
+        } else {
+            view.setBackgroundColor(getResources().getColor(R.color.green));
+        }
+        zeroButton.setBackgroundColor(getResources().getColor(R.color.purple_200));
     }
 }
