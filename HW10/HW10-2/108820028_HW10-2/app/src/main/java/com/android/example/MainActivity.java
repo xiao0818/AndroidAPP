b10c9fd967b9731293556de7800f4a41d24e92ac
Eddie <kta23379745@gmail.com>
2022-05-03 11:47:29 +0800

    HW10-2ing...

--- HW10/HW10-2/108820028_HW10-2/app/src/main/java/com/android/example/MainActivity.java
+++ HW10/HW10-2/108820028_HW10-2/app/src/main/java/com/android/example/MainActivity.java
@@ -2,13 +2,65 @@ package com.android.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private EditText mUrlEditText;
+    private TextView mResultTextView;
+    private String spinnerLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUrlEditText = (EditText) findViewById(R.id.url_edit_text);
+        mResultTextView = (TextView) findViewById(R.id.result_text_view);
+
+        // ... Rest of onCreate code ...
+        // Create the spinner.
+        Spinner spinner = findViewById(R.id.url_head_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+        }
+        // Create ArrayAdapter using the string array and default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.url_head, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner.
+        if (spinner != null) {
+            spinner.setAdapter(adapter);
+        }
+        // ... End of onCreate code ...
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    // Interface callback for when any spinner item is selected.
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        spinnerLabel = adapterView.getItemAtPosition(i).toString();
+    }
+
+    // Interface callback for when no spinner item is selected.
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        // Do nothing.
+    }
+
+    public void getPageSource(View view) {
+        String queryString = spinnerLabel + mUrlEditText.getText().toString();
+//        displayToast(queryString);
+        new FetchWeb(mResultTextView).execute(queryString);
     }
 }
---
+++ HW10/HW10-2/108820028_HW10-2/app/src/main/java/com/android/example/NetworkUtils.java
@@ -0,0 +1,59 @@
+package com.android.example;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class NetworkUtils {
+    private static final String LOG_TAG = NetworkUtils.class.getSimpleName();
+
+    static String getWebInfo(String queryString){
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String webString = null;
+
+        try {
+            URL requestURL = new URL(queryString);
+            urlConnection = (HttpURLConnection) requestURL.openConnection();
+
+            // Get the InputStream.
+            InputStream inputStream = urlConnection.getInputStream();
+            // Create a buffered reader from that input stream.
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            // Use a StringBuilder to hold the incoming response.
+            StringBuilder builder = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append("\n");
+            }
+            if (builder.length() == 0) {
+                return null;
+            }
+            webString = builder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Check your internet connection and try again.";
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        Log.d(LOG_TAG, webString);
+        return webString;
+    }
+}
