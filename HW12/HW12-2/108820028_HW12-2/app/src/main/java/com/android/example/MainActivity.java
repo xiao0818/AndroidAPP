3c92d0391859785f39787afe23fd4b9b1fb5dc34
Eddie <kta23379745@gmail.com>
2022-05-04 22:09:38 +0800

    prepare for HW13

--- HW12/HW12-2/108820028_HW12-2/app/src/main/java/com/android/example/MainActivity.java
+++ HW12/HW12-2/108820028_HW12-2/app/src/main/java/com/android/example/MainActivity.java
@@ -1,14 +1,101 @@
 package com.android.example;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.job.JobInfo;
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int JOB_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    NotificationManager mNotifyManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+    }
+
+    public void Download(View view) {
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        ComponentName componentName = new ComponentName(getApplicationContext(), NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, componentName);
+        builder.setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setRequiresDeviceIdle(true)
+                .setPeriodic(TimeUnit.DAYS.toMillis(1));
+        if (Build.VERSION.SDK_INT > 23) {
+            builder.setPeriodic(TimeUnit.DAYS.toMillis(1), TimeUnit.MINUTES.toMillis(5));
+        }
+        JobInfo jobInfo = builder.build();
+        if (scheduler != null) {
+            scheduler.schedule(jobInfo);
+        }
+        sendDownloadNotification();
+    }
+
+    public class NotificationJobService extends JobService {
+        @Override
+        public boolean onStartJob(JobParameters jobParameters) {
+            sendDownloadNotification();
+            return false;
+        }
+
+        @Override
+        public boolean onStopJob(JobParameters jobParameters) {
+            return true;
+        }
+    }
+
+    public void sendDownloadNotification() {
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (this, 0, new Intent(this, MainActivity.class),
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder
+                (this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("Performing Work")
+                .setContentText("Download in progress")
+                .setContentIntent(contentPendingIntent)
+                .setSmallIcon(R.drawable.ic_job_running_foreground)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                .setAutoCancel(true);
+
+        mNotifyManager.notify(0, builder.build());
+    }
+
+    public void createNotificationChannel() {
+        mNotifyManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Job Service notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription
+                    ("Notifications from Job Service");
+
+            mNotifyManager.createNotificationChannel(notificationChannel);
+        }
     }
 }
